212d662d605aef4989c6e46252d4d0dab4da98f3
James Hunt <james.jh.hunt@gmail.com>
2025-05-13 11:37:34 +1000

    Adding Macro & Micro tests for move & precision

--- src/main.py
+++ src/main.py
@@ -7,6 +7,31 @@ def main():
     """
     Main function to run the move test.
     """
+
+    absMoveTestMicro = MoveTestAbsolute(test_name="Absolute Move Test Micro", posn=10.005, precision=0.00025)
+    absMoveTestMicroResult = absMoveTestMicro.execute(motor=1, encoder=9)
+    print(absMoveTestMicroResult)
+
+    absMoveTestMacro = MoveTestAbsolute(test_name="Absolute Move Test Macro", posn=15, precision=0.00025)
+    absMoveTestMacroResult = absMoveTestMacro.execute(motor=1, encoder=9)
+    print(absMoveTestMacroResult)
+
+    absMoveTestMacroPrecise = MoveTestAbsolute(test_name="Absolute Move Test Macro Precise", posn=25.005, precision=0.00025)
+    absMoveTestMacroPreciseResult = absMoveTestMacroPrecise.execute(motor=1, encoder=9)
+    print(absMoveTestMacroPreciseResult)
+
+    precisionTestMicro = PrecisionTest(test_name="Precision Test Micro", startPosn=10, endPosn=10.005, precision=0.00025)
+    precisionTestMicroResult = precisionTestMicro.execute(motor=1, encoder=9)
+    print(precisionTestMicroResult)
+
+    precisionTestMacro = PrecisionTest(test_name="Precision Test Macro", startPosn=10, endPosn=20, precision=0.00025)
+    precisionTestMacroResult = precisionTestMacro.execute(motor=1, encoder=9)
+    print(precisionTestMacroResult)
+
+    precisionTestMicroLarge = PrecisionTest(test_name="Precision Test Micro Large", startPosn=10, endPosn=20.005, precision=0.00025)
+    precisionTestMicroLargeResult = precisionTestMicroLarge.execute(motor=1, encoder=9)
+    print(precisionTestMicroLargeResult)
+
     # Example usage
     print("Test 1")
     test = MoveTestAbsolute(test_name="Test 1", posn=10.005, precision=0.00025)
