657e47fdc0c9c243a038f0c02a2ca52d6307002a
James Hunt <james.jh.hunt@gmail.com>
2025-05-25 18:26:09 +1000

    Updating Overshoot test to accept explicit velocity

--- src/controllerTest/OvershootTest.py
+++ src/controllerTest/OvershootTest.py
@@ -5,52 +5,39 @@ import time
 
 class OvershootTest(MotionControlTest):
 
-    def __init__(self, test_name: str, precision: float = 0.001, distance: float = 10):
+    def __init__(self, test_name: str, velocity: float, controller: Controller, distance: float = 10, precision: float = 0.001):
             super().__init__(test_name, "Overshoot Test")
             self.precision = precision
             self.distance = distance
+            self.velocity = velocity
+            self.controller = controller
 
     def execute(self, motor: int, encoder: int):
         #connect to the controller
-        controller = Controller(host="10.23.231.3")
-        controller.connect()
+        controller = self.controller
         
-        #variables
-        speeds = {'slow': 0.001, 'medium': 0.0025, 'fast': 0.005}
+        controller.set_velocity(motor, self.velocity)
+        #start moving
+        controller.move_to_pos(motor, self.distance)
+        peak_position = 0
+        inpos_state = controller.in_pos(motor)
+        while (inpos_state) != 1:
+            pos = controller.get_pos(encoder)
+            peak_position = max(peak_position, pos)
+            inpos_state =  controller.in_pos(motor)
+            #time.sleep(0.05)
+        # calculate the overshoot
+        overshoot = peak_position - self.distance
         
-        #move to 0 position
+        #move back to 0 position
         controller.move_to_pos_wait(motor, 0)
         
-        #loop through different speeds
-        for speed in speeds:
-            #set the speed
-            controller.set_velocity(motor, speeds[speed])
-            #start moving
-            controller.move_to_pos(motor, self.distance)
-            peak_position = 0
-            inpos_state = controller.in_pos(motor)
-            while (inpos_state) != 1:
-                pos = controller.get_pos(encoder)
-                peak_position = max(peak_position, pos)
-                inpos_state =  controller.in_pos(motor)
-                #time.sleep(0.05)
-            # calculate the overshoot
-            overshoot = peak_position - self.distance
-            print(f"Speed: {speed}, Overshoot: {overshoot}")
-            
-            #move back to 0 position
-            controller.move_to_pos_wait(motor, 0)
-            
-            
-            #check if the overshoot is within the precision
-            if overshoot > self.precision:
-                print(f"Overshoot test failed at speed {speed}. Overshoot: {overshoot}")
-                result = MotionControlResult(success=False, message=f"Overshoot test failed at speed {speed}. Overshoot: {overshoot}")
-                return result
-                
-            else:
-                print(f"Overshoot test passed at speed {speed}. Overshoot: {overshoot}")
-                result = MotionControlResult(success=True, message=f"Overshoot test passed at speed {speed}. Overshoot: {overshoot}")
+        
+        #check if the overshoot is within the precision
+        if overshoot > self.precision:
+            result = MotionControlResult(success=False, message=f"Overshoot test failed at speed {self.velocity}. Overshoot: {overshoot}")
+        else:
+            result = MotionControlResult(success=True, message=f"Overshoot test passed at speed {self.velocity}. Overshoot: {overshoot}")
                 
         return result
         
